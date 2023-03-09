edc3e367120f311534aadc85a7dbb942289cd805
Herjus <herjus420@gmail.com>
2023-03-09 10:11:16 +0100

    n

--- post.js
+++ post.js
@@ -1,11 +1,11 @@
-res = fetch('http://localhost:8888/messages', {
+res = fetch('http://20.238.117.226:8888/messages', {
     method: 'POST',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-        "name": "jacob",
-        "message": "taper^^"
+        "name": "Henrik",
+        "message": "legende"
     })
 })
