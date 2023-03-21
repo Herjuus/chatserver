33caf027b18daf18703cbb410f1ae5a331e8ad15
Herjus <119409948+Herjuus@users.noreply.github.com>
2023-03-21 12:54:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 // const dbConnectionString = process.env.DB_CONNECTION_STRING;
 
 mongoose
-  .connect('mongodb+srv://herjus:passord@cluster0.d94ocoy.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect('mongodb+srv://link', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err));
 
